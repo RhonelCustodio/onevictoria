2103c3f6c9485b7d6a7bb629924450eb26729aeb
Rhonel Allen A. Custodio <ra.custodio00587@student.tsu.edu.ph>
2026-08-13 15:48:46 +0800

    Add Firebase configuration file

---
+++ firebase-config/firebase-config.js
@@ -0,0 +1,10 @@
+// ===== FIREBASE INITIALIZATION =====
+export const firebaseConfig = {
+  apiKey: "AIzaSyD6rgutkYK7MZ3F0Xne6Zs4PyEiPME7ePM",
+  authDomain: "onevictoria-23409.firebaseapp.com",
+  projectId: "onevictoria-23409",
+  storageBucket: "onevictoria-23409.firebasestorage.app",
+  messagingSenderId: "334731169631",
+  appId: "1:334731169631:web:7484599232fef8b06eb0ea",
+  measurementId: "G-0ML9K6JSK8",
+};
